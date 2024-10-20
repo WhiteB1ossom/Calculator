0627ef1a0ee6f911ff33fa3e88dad3d4ae9c3861
Blossom <blossom5831@naver.com>
2024-10-20 23:23:36 +0900

    계산기 프로그램을 만들 때, 버튼 생성 코드를 중복으로 사용하지 않기 위해 배열과 향상된 for문을 사용하였고 index라는 int 변수를 할당하여 for문에서 index를 사용할 수 있게끔 하였습니다. index는 추후 버튼의 background color 설정 때 사용할 예정입니다.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,47 @@
+import java.awt.Color;
+import java.awt.GridLayout;
 
-public class Calculator {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Calculator extends JFrame{
+
+	//버튼에 들어갈 문자들 배열
+	String[] Btn = {
+			"%", "CE", "C", "←",
+			"¹/x", "x²", "²√x", "÷",
+			"7", "8", "9", "x",
+			"4", "5", "6", "-",
+			"1", "2", "3", "+",
+			"+/-", "0", ".", "="
+			};
+	
+	JButton[] b = new JButton[Btn.length]; //버튼 생성 및 버튼 개수 설정
+	int index = 0; //향상된 for문에서 쓰일 b의 인덱스
+	
+	JPanel p;
+	
+	Calculator() {
+		this.setTitle("계산기");
+		this.setSize(520,720);
+		
+		//계산기 모양으로 GridLayout 설정
+		p = new JPanel(new GridLayout(6, 4, 2, 2));
+		
+		//버튼 생성 for문
+		for(String text : Btn) {
+			b[index] = new JButton(text);
+			b[index].setBackground(Color.white);
+			p.add(b[index]);
+		}
+		
+		add(p);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("Real Test Commit");
+		new Calculator();
 	}
 }
